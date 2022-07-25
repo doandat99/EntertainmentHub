96a533b6488d2aeb9b7fc4f03afb339081bb97ad
doandat99 <dsky5463@gmail.com>
2022-07-26 05:25:29 +0700

    push

--- src/component/Hooks/useGenres.js
+++
@@ -1,5 +0,0 @@
-export const useGenre = (selectedGenres) => {
-  if (selectedGenres.length < 1) return "";
-  const id = selectedGenres.map((item) => item.id);
-  return id.reduce((acc, arr) => acc + "," + arr);
-};
--- src/src/hooks/useGene.js
+++
@@ -1,5 +0,0 @@
-export const useGene = (selectedGenres) => {
-  if (selectedGenres.length < 1) return "";
-  const GenreIds = selectedGenres.map((g) => g.id);
-  return GenreIds.reduce((acc, curr) => acc + "," + curr);
-};
